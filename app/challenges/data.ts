934bb0b77033fc47642b6755abff8583c27314fa
easyvipin <vipinchandra555@gmail.com>
2025-01-16 22:55:09 +0530

    feat: added challenge page

--- app/challenges/data.ts
+++ app/challenges/data.ts
@@ -1,8 +1,15 @@
 const ALL_CHALLANGES = [
   {
     id: "auto-complete-react",
+    label: "React Autocomplete Search",
     codeBase: "https://codesandbox.io/p/sandbox/auto-complete-react-m5q4vh",
     liveUrl: "https://m5q4vh.csb.app/",
+  },
+  {
+    id: "use-timeout-hook",
+    label: "useTimeout() hook in React",
+    codeBase: "https://codesandbox.io/p/sandbox/usetimeout-gh27mj",
+    liveUrl: "https://gh27mj.csb.app/",
   },
 ];
 
---
+++ app/challenges/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ALL_CHALLANGES from "./data";
+import { VscVscode } from "react-icons/vsc";
+import { RiNavigationLine } from "react-icons/ri";
+import Link from "next/link";
+
+interface IUIChallengesProps {}
+
+const UIChallenges: React.FunctionComponent<IUIChallengesProps> = (props) => {
+  return (
+    <div className="w-[95vw] md:w-[65vw] mx-auto py-8 md:py-10">
+      <h4 className="text-2xl text-center font-extralight text-gray-500 mb-4">
+        UI SANDBOXES
+      </h4>
+      <div className="flex flex-col gap-4">
+        {ALL_CHALLANGES.map((eachChallenge) => {
+          return (
+            <div
+              key={eachChallenge.id}
+              className="flex justify-between items-center px-4 py-2 border shadow-md rounded-md"
+            >
+              <div>
+                <h4>{eachChallenge.label}</h4>
+              </div>
+              <div className="flex gap-4">
+                <Link href={eachChallenge.codeBase}>
+                  <VscVscode />
+                </Link>
+                <Link href={eachChallenge.liveUrl}>
+                  <RiNavigationLine />
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default UIChallenges;
